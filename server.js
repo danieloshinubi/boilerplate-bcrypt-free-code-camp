a8395718d465aece5ce6a12662258ebb4c738d00
Daniel Oshinubi <danieloshinubi@gmail.com>
2023-12-24 00:43:04 -0400

    adding bcrypt to the server

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 'use strict';
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const app         = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const app = express();
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
